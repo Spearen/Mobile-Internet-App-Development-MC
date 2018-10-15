c5154a5a495bed8eeeb47915526833638733d80d
MARK CUNNINGHAM <G00365259@gmit.ie>
2018-10-15 10:06:02 +0100

    test

---
+++ LoginApp/LoginApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+
+        }
+
+
+
+        
+       
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+
+
+
+            bool isUserEmpty = String.IsNullOrEmpty(entryUsername.Text);//checks if the text field is empty
+            bool isPassEmpty = String.IsNullOrEmpty(entryPassword.Text);
+
+            if ((isUserEmpty) || (isPassEmpty))
+            {
+                //stop user logging in
+            }
+
+            else
+            {
+                //go to new page
+                Navigation.PushAsync(new Page1());
+            }
+
+
+        }
+    }
+}
---
+++ LoginApp/LoginApp/Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : TabbedPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+
+            ToolbarItem current = (ToolbarItem)sender;
+            if(current.Text == "Add")
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
